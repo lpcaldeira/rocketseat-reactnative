e39072ee56c5c10740487ddca3be58198e32665f
lpcaldeira <leonardo.caldeira@mysmartclinic.com.br>
2020-03-24 23:12:10 -0300

    Estilizando a aplicação

--- rocketseatreactnative/src/pages/main.js
+++ rocketseatreactnative/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { TouchableOpacity, View, Text, FlatList } from 'react-native'
+import { TouchableOpacity, View, Text, FlatList, StyleSheet } from 'react-native'
 import api from '../services/api'
 
 export default class Main extends Component {
@@ -26,20 +26,22 @@ export default class Main extends Component {
     }
 
     renderItem = ({ item }) => (
-        <View>
-            <Text>{item.title}</Text>
-            <Text>{item.description}</Text>
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.title}</Text>
+            <Text style={styles.productDescription}>{item.description}</Text>
             <TouchableOpacity
+                style={styles.productButton}
                 onPress={() => { }}>
-                <Text>Acessar</Text>
+                <Text style={styles.productButtonText}>Acessar</Text>
             </TouchableOpacity>
         </View>
     )
 
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <FlatList
+                    contentContainerStyle={styles.list}
                     data={this.state.products}
                     keyExtractor={item => item._id}
                     renderItem={this.renderItem}
@@ -48,3 +50,48 @@ export default class Main extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fafafa',
+
+    },
+    list: {
+        padding: 20
+    },
+    productContainer: {
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#DDD',
+        borderRadius: 5,
+        padding: 20,
+        marginBottom: 20
+    },
+    productTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+    productDescription: {
+        fontSize: 16,
+        color: '#999',
+        marginTop: 5,
+        lineHeight: 24
+    },
+    productButton: {
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#DA552F',
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    },
+    productButtonText: {
+        fontSize: 16,
+        color: '#DD552F',
+        fontWeight: 'bold'
+    }
+})
